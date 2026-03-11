23a26d5c917015b00484c6335c7ed58ca8e9fdec
Jeayam <jlivanno@student.ciputra.ac.id>
2026-03-11 15:47:10 +0800

    selesaii

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class App {
+public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
